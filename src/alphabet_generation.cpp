498b7b1e6db225d6ef066883a4fac73ab694d097
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-12 02:07:06 +0700

    Wrote_alph_gen.hpp

---
+++ src/alphabet_generation.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int generation_alphabet(
+        string* alphabet,
+        int* length_alphabet,
+        bool take_digit_flag,
+        bool take_small_symbol_flag,
+        bool take_large_symbol_flag,
+        bool take_special_symbol_flag);
+        
