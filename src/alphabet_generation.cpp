712e462a2411b0b30ad69ec40411490435aefbd8
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-12 02:09:10 +0700

    Wrote_alphabet_generation.cpp

--- src/alphabet_generation.cpp
+++ src/alphabet_generation.cpp
@@ -1,11 +1,10 @@
-#pragma once
+#include "alphabet_generation.hpp"
 
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <string>
 
-using namespace std;
+const string digit_alphabet = "0123456789";
+const string small_character = "abcdefghijklmnopqrstuvwxyz";
+const string large_character = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string special_symbol = "!@#$^&*?";
 
 int generation_alphabet(
         string* alphabet,
@@ -13,5 +12,29 @@ int generation_alphabet(
         bool take_digit_flag,
         bool take_small_symbol_flag,
         bool take_large_symbol_flag,
-        bool take_special_symbol_flag);
-        
+        bool take_special_symbol_flag)
+{
+    if (take_digit_flag) {
+        *alphabet += digit_alphabet;
+    }
+    
+    if (take_small_symbol_flag) {
+        *alphabet += small_character;
+    }
+
+    if (take_large_symbol_flag) {
+        *alphabet += large_character;
+    }
+
+    if (take_special_symbol_flag) {
+        *alphabet += special_symbol;
+    }
+
+    *length_alphabet = alphabet->length();
+
+    if (*alphabet == "") {
+        *length_alphabet = 0;
+        return 1;
+    }
+    return 0;
+}
