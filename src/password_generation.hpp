0b5b83caa206534becb505a90770cbd35719675a
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-12 13:06:40 +0700

    Wrote_header_pg.hpp

---
+++ src/password_generation.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+int password_generation(
+        string **passwords,
+        int len_password,
+        int numb_passwords,
+        string* alphabet,
+        int length_alphabet);
