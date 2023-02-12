4db5c42da273f011202127f25556bbed21a11594
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-12 13:29:17 +0700

    Wrote_password_generation_and_output_function

---
+++ src/password_generation.cpp
@@ -0,0 +1,22 @@
+#include "password_generation.hpp"
+
+void print_passwords(string* passwords, int len_password, int numb_passwords) {
+    for(int i = 0; i < numb_passwords; i++) {
+        cout << passwords[i] << endl;
+    }
+}
+
+int password_generation(string* passwords, int len_password, int numb_passwords,string* alphabet, int length_alphabet) {
+
+    if(passwords == nullptr) {
+        return 1;
+    }
+
+    srand(time(nullptr));
+    for(int i = 0; i < numb_passwords; i++) {
+        for(int j = 0; j < len_password; j++) {
+            passwords[i] += alphabet[rand() % length_alphabet];
+        }
+    }
+    return 0;
+}
--- src/password_generation.hpp
+++ src/password_generation.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+#include <cstdlib>
 
-int password_generation(
-        string **passwords,
-        int len_password,
-        int numb_passwords,
-        string* alphabet,
-        int length_alphabet);
+using namespace std;
+
+int password_generation(string** passwords, int len_password, int numb_passwords, string* alphabet, int length_alphabet);
+void print_passwords(string** passwords, int len_password, int numb_passwords);
